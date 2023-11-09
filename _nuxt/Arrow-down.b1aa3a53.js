7c109f89557ea9b4e7dd327a1de9a0cc804133a5
tmilost <tmilos94@gmail.com>
2023-11-09 16:39:33 +0100

    Updates

--- _nuxt/Arrow-down.b1aa3a53.js
+++
@@ -1,1 +0,0 @@
-import"./entry.0016ea3a.js";const o=""+globalThis.__publicAssetsURL("Arrow-down.svg");export{o as _};
---
+++ assets/Arrow-down.f13d5c2a.js
@@ -0,0 +1,1 @@
+import"./entry.4791c0b3.js";const o=""+new URL("Arrow-down.b9dfe3f0.svg",import.meta.url).href;export{o as _};
